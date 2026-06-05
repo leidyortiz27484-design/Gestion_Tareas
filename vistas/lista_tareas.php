e2612d1e5dc1c98704dd7e7c04706c62242dfdf9
unknown <leidyortiz27484@gmail.com>
2026-06-05 18:51:22 -0500

    Fix: Forzar la recarga del CSS en el navegador usando control de versiones

--- vistas/lista_tareas.php
+++ vistas/lista_tareas.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestión de Tareas</title>
-    <link rel="stylesheet" href="estilos.css">
+    <!--<link rel="stylesheet" href="estilos.css">-->
+    <link rel="stylesheet" href="estilos.css?v=1.1">
+
 </head>
 <body>
 
